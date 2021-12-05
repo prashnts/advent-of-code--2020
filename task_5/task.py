001919d96909d0ece174f0ab9d756079f8e27bf3
Prashant Sinha <prashant@noop.pw>
2021-12-05 17:44:29 +0100

    Add task 5 solution

--- task_5/task.py
+++ task_5/task.py
@@ -1,8 +1,5 @@
 import os
 
-from itertools import combinations
-
-
 __here__ = os.path.dirname(__file__)
 
 TEST_DATA = '''\
@@ -14,29 +11,58 @@ TEST_DATA = '''\
 1456\
 '''
 
+def split(arr):
+    mid = len(arr) // 2
+    return arr[:mid], arr[mid:]
+
+
+def seat_id(path):
+    row_space = list(range(128))
+    col_space = list(range(8))
+    current_row = row_space
+    current_col = col_space
+
+    for direction in path[:7]:
+        lower, upper = split(current_row)
+        if direction == 'F':
+            current_row = lower
+        elif direction == 'B':
+            current_row = upper
+
+    for direction in path[7:]:
+        lower, upper = split(current_col)
+        if direction == 'L':
+            current_col = lower
+        elif direction == 'R':
+            current_col = upper
+
+    row = current_row[0]
+    column = current_col[0]
+
+    return (row * 8) + column
+
+
 def calculate_1(data):
-    nums = [int(x) for x in data.split('\n')]
-
-    pairs = combinations(nums, 2)
-
-    for x, y in pairs:
-        if x + y == 2020:
-            return x * y
+    paths = data.split('\n')
+    seat_ids = [seat_id(path) for path in paths]
+    return max(seat_ids)
 
 
 def calculate_2(data):
-    nums = [int(x) for x in data.split('\n')]
+    paths = data.split('\n')
+    seat_ids = [seat_id(path) for path in paths]
 
-    triples = combinations(nums, 3)
+    all_seats = set(range(min(seat_ids), max(seat_ids) + 1))
 
-    for x, y, z in triples:
-        if x + y + z == 2020:
-            return x * y * z
+    remaining_seats = all_seats - set(seat_ids)
+    return remaining_seats.pop()
 
 
 if __name__ == '__main__':
-    assert calculate_1(TEST_DATA) == 514579
-    assert calculate_2(TEST_DATA) == 241861950
+    assert seat_id('FBFBBFFRLR') == 357
+    assert seat_id('BFFFBBFRRR') == 567
+    assert seat_id('FFFBBBFRRR') == 119
+    assert seat_id('BBFFBBFRLL') == 820
 
     with open(os.path.join(__here__, 'input.txt'), 'r') as fp:
         data = fp.read()
