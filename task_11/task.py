8ec9391336cb0d7d80ec3022325d5af560245019
Prashant Sinha <prashant@noop.pw>
2021-12-11 17:18:04 +0100

    Task 11

--- task_11/task.py
+++ task_11/task.py
@@ -1,4 +1,5 @@
 import os
+from functools import lru_cache
 
 
 __here__ = os.path.dirname(__file__)
@@ -68,20 +69,75 @@ def calculate_1(data):
 
 
 def calculate_2(data):
+    '''Almost a brute-force attempt without optimisations
+    Converges after ~80 iterations.
+    '''
     layout = [list(row) for row in data.split('\n')]
+    sx, sy = len(layout), len(layout[0])
     state = [r[:] for r in layout]
+    directions = ['up', 'dn', 'lt', 'rt', 'nw', 'ne', 'sw', 'se']
+
+    if sx > 50:
+        print('[Part 2] Hang tight! This will take a little while to converge.')
+
+    @lru_cache
+    def coords_in_dirn(dirn, cx, cy):
+        if dirn == 'up':
+            # going up means to go from x to 0, keeping y same. (in our system)
+            xsteps = [cx - i for i in range(1, sx) if cx - i >= 0]
+            coords = [(x, cy) for x in xsteps]
+        if dirn == 'dn':
+            # goin down means to go from x to sx, keeping y same.
+            xsteps = [cx + i for i in range(1, sx) if cx + i < sx]
+            coords = [(x, cy) for x in xsteps]
+        if dirn == 'lt':
+            # going left means to go from y to 0. Keeping x same.
+            ysteps = [cy - i for i in range(1, sy) if cy - i >= 0]
+            coords = [(cx, y) for y in ysteps]
+        if dirn == 'rt':
+            # going right means to go from y to sy. Keeping x same.
+            ysteps = [cy + i for i in range(1, sy) if cy + i < sy]
+            coords = [(cx, y) for y in ysteps]
+        if dirn == 'nw':
+            # going north-west means to decrease x and y together.
+            xsteps = [cx - i for i in range(1, sx) if cx - i >= 0]
+            ysteps = [cy - i for i in range(1, sy) if cy - i >= 0]
+            coords = zip(xsteps, ysteps)
+        if dirn == 'ne':
+            # going north-east means to decrease x and increase y together.
+            xsteps = [cx - i for i in range(1, sx) if cx - i >= 0]
+            ysteps = [cy + i for i in range(1, sy) if cy + i < sy]
+            coords = zip(xsteps, ysteps)
+        if dirn == 'sw':
+            # going south-west means to increase x and decrease y together.
+            xsteps = [cx + i for i in range(1, sx) if cx + i < sx]
+            ysteps = [cy - i for i in range(1, sy) if cy - i >= 0]
+            coords = zip(xsteps, ysteps)
+        if dirn == 'se':
+            # going south-east means to increase x and y together.
+            xsteps = [cx + i for i in range(1, sx) if cx + i < sx]
+            ysteps = [cy + i for i in range(1, sy) if cy + i < sy]
+            coords = zip(xsteps, ysteps)
+        yield from coords
 
     while True:
         next_state = [r[:] for r in state]
 
-        for x in range(len(state)):
-            for y in range(len(state[0])):
+        for x in range(sx):
+            for y in range(sy):
                 curr = state[x][y]
-                neighbors = list(gen_neighbors(state, x, y))
+                neighbors = []
+
+                for dirn in directions:
+                    for cx, cy in coords_in_dirn(dirn, x, y):
+                        if state[cx][cy] == '.':
+                            continue
+                        neighbors.append(state[cx][cy])
+                        break
 
                 if curr == 'L' and neighbors.count('#') == 0:
                     next_state[x][y] = '#'
-                elif curr == '#' and neighbors.count('#') >= 4:
+                elif curr == '#' and neighbors.count('#') >= 5:
                     next_state[x][y] = 'L'
 
         if state == next_state:
